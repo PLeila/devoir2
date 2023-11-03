6a8ec5ee5af7177ff6106c270845920476375491
PLeila <watcherleyplonquette@gmail.com>
2023-11-03 10:23:41 -0400

    first commit

---
+++ devoir2.py
@@ -0,0 +1,12 @@
+#1
+phrase=("DIEU EST TOUT POUR MOI")
+print (phrase.lower())
+#2
+cutcaracters=("hello we are here to practice python")
+print(cutcaracters.split())
+#3
+firstletter=("happy birthday leila")
+print(firstletter.title())
+
+#4   
+
